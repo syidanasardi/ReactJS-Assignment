6451c0783f6ac1209cb75d21405bb4cde1a74b58
syidanasardi <syidan.asardi@gmail.com>
2024-01-08 19:58:47 +0700

    edited

--- note-manager/src/App.jsx
+++ note-manager/src/App.jsx
@@ -2,9 +2,15 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+
+function Notes({notes, toggleImportance}) {
+  const label = notes['important'] ? 'make not important' : 'make important'
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState('')
+  const [showAll, setShowAll] = useState(true)
   const [notes, setNotes] = useState()
 
   return (
