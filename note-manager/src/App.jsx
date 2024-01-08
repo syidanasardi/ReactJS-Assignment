81a0286fbf614c61d779f04ed6507cfb0381b97b
syidanasardi <syidan.asardi@gmail.com>
2024-01-08 19:24:06 +0700

    Added Files

---
+++ note-manager/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [notes, setNotes] = useState()
+
+  return (
+    <>
+      <h1 className='text-5xl'>Hello World</h1>
+
+    </>
+  )
+}
+
+export default App
