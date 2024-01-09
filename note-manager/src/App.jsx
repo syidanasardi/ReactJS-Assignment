dc6e9e3a36f1c52b7ef1254be2aeba5fda017e1e
syidanasardi <syidan.asardi@gmail.com>
2024-01-09 09:59:14 +0700

    updated

--- note-manager/src/App.jsx
+++ note-manager/src/App.jsx
@@ -4,25 +4,54 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
 
-function Notes({notes, toggleImportance}) {
-  <div>
-    <p>{notes}</p>
-  </div>
+function Note({note}) {
+    return (
+      <li>{note['content']}</li>
+    )
 }
 
 function App() {
-  const [count, setCount] = useState('')
   const [showAll, setShowAll] = useState(true)
-  const [notes, setNotes] = useState()
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
 
+  useEffect(() => {
+    axios.get('http://localhost:3000/notes')
+    .then(response => {setNotes(response['data'])})
+  }, [])
+
+  function addNote(event) {
+    event.preventDefault()
+    const noteObject = {
+      id : notes.length + 1,
+      content : newNote
+    }
+
+    axios.post('http://localhost:3000/notes', noteObject)
+    .then(response => {
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+    })
+  }
+
+  function handleFormCHange(event) {
+    setNewNote(event.target.value)
+  }
 
 
   return (
-    <>
-      <h1 className='text-5xl'>Hello World</h1>
-      
-
-    </>
+    <div>
+      <h1 className='text-5xl'>Notes</h1>
+      <ul className='text-left'>
+      {notes.map(note => 
+          <Note key={note['id']} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote} className='text-left'>
+        <input value={newNote} onChange={handleFormCHange}/>
+        <button type='submit'>save</button>
+      </form>
+    </div>
   )
 }
 
