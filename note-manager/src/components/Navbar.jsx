17aecd2bfdf09b8a2d197133384a1847f9ad1407
syidanasardi <syidan.asardi@gmail.com>
2024-01-11 16:05:20 +0700

    Link feature added

--- note-manager/src/components/Navbar.jsx
+++ note-manager/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
+import {Link} from 'react-router-dom'
+
 function Navbar() {
     return (
         <nav className="sticky top-0 bg-red-100">
             <p className='text-5xl'>My Notes</p>
-            <a href="/"><button className='text-3xl absolute top-2 right-1/4'>Home</button></a>
-            <a href="/create"><button className='text-3xl absolute top-2 right-1/3'>Add Note</button></a>
+            <Link to="/" className='text-3xl absolute top-2 right-1/4'>Home</Link>
+            <Link to="/create" className='text-3xl absolute top-2 right-1/3'>Add Note</Link>
         </nav>
     )
 }
